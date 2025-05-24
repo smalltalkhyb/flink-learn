1412dbd9a7ab448e12c768a0be42224204b835c9
hyb-stone <297385780@qq.com>
2025-05-24 14:48:29 +0800

    新增flink-sql 项目

---
+++ flink-sql/src/main/java/com/hyb/flink/sql/FlinkSqlApplication.java
@@ -0,0 +1,13 @@
+package com.hyb.flink.sql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlinkSqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlinkSqlApplication.class, args);
+    }
+
+}
